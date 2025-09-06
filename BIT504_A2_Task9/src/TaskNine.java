e7c5708b6e4e3342267014f291700ac40c4bf1b0
BIT504 <ben.rewi@icloud.com>
2025-09-06 21:46:29 +1000

    I changed the increase in value for the multiplier from 10 to 90

--- BIT504_A2_Task9/src/TaskNine.java
+++ BIT504_A2_Task9/src/TaskNine.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class TaskNine {
 
     public static void main(String[] args) {
+    	
         Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Enter a number: ");
+        
         double number = scanner.nextDouble();
-        double result = number * (number + 10);
+        
+        
+        //changing the number to 90!
+        double result = number * (number + 90);
+        
         System.out.println(number + " × (" + number + " + 10) = " + result);
+        
         scanner.close();
     }
 }
